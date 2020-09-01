49d7d991c5b038f5462194fe6b1f8cb20f3e331e
msund385 <ms43004foru@gmail.com>
2020-09-01 16:31:37 -0400

    Change todos

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -7,48 +7,52 @@ namespace IterationStatements
     {
         static void Main(string[] args)
         {
-            //TODO
+           
 
-            //Create a List called "numbers" - DONE
-            var numbers = new List<int>();
             
-            //Create a variable of type int and initialize the variable with a value of 0
+            var numbers = new List<int>();
+
+            
+            int i = 0;
 
 
-
-            // Create a do-while loop
+            do
             {
-                // Increment your variable by 1
+                i++;
+                numbers.Add(i);
 
-                // Then add your variable to "numbers"
-                numbers.Add(/* variable name goes here */);
-            }
-            // While your variable is less than 100
+            } while (i <= 100);
 
 
-
-            // Create a while loop
-            // While your variable is less than 200
+            while (i <= 200)
             {
-                // Increment your variable by 1
-                // Then add your variable to "numbers"
+                i++;
+                numbers.Add(i);
+                
             }
 
             Console.WriteLine("Increase:");
 
-            // Create a foreach loop
-            // Write your variable to the console
 
-            Console.WriteLine("");
-            Console.WriteLine("Decrease:");
 
-            // Create a for loop
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+            
+
+           Console.WriteLine("");
+           Console.WriteLine("Decrease:");
+
+           for (i = 199; i <= numbers.Count; i--)
+           while(i >= 0)
             // in your initializer set the value of i to 199
-            // in your conditional, as long as i is less than or equal to the amount of items in "numbers" (numbers.Count)
+            //in your conditional, as long as i is less than or equal to the amount of items in "numbers" (numbers.Count)
             // and as long as i is greater than or equal to 0
             // Decrement i by 1
             {
-                // Write to the console "numbers" at index i
+               Console.WriteLine(numbers[1]);
             }
         }
     }
